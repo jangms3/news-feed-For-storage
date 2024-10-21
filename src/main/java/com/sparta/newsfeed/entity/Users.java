800e89be885764911a136f5fb77467aded7325ec
eun0 <shinely12@gmail.com>
2024-10-21 19:37:46 +0900

    Timestamped 클래스명 정정

--- src/main/java/com/sparta/newsfeed/entity/Users.java
+++ src/main/java/com/sparta/newsfeed/entity/Users.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Users extends Timestamp {
+public class Users extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
