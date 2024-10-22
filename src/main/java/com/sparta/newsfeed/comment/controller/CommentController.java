f02c93f054b8696e08ccacd1ea8a73bd0366cce3
jangms3 <github.com/jangms3>
2024-10-22 11:08:42 +0900

    api 변경

--- src/main/java/com/sparta/newsfeed/comment/controller/CommentController.java
+++ src/main/java/com/sparta/newsfeed/comment/controller/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/feeds/{feedId}/comments")
+@RequestMapping("api/feeds/{feedId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
